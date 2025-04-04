c0f2a472f5a5a07bf56ee3338a27646e294063b8
voloide <voloidet@gmail.com>
2025-04-04 10:40:10 +0200

    Version bump to 1.0.11

--- app/src/main/java/mz/org/csaude/mentoring/base/application/MentoringApplication.java
+++ app/src/main/java/mz/org/csaude/mentoring/base/application/MentoringApplication.java
@@ -126,7 +126,7 @@ public class MentoringApplication  extends Application {
 
     private static MentoringApplication mInstance;
     public static final String BASE_URL = "https://mentdev.csaude.org.mz/api/";
-//    public static final String BASE_URL = "http://10.10.12.39:8087/api/";
+    //public static final String BASE_URL = "http://10.10.12.115:8087/api/";
     //public static final String BASE_URL = "http://192.168.1.32:8087/api/";
     private User authenticatedUser;
 
